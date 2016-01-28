09b4587e7b0235876e29c56a6853a7d02c534e0f
Doug <dougnelson@silktide.com>
2016-01-28 17:29:44 +0000

    added concept of sendtimeout

--- src/SocketFactory.php
+++ src/SocketFactory.php
@@ -7,17 +7,22 @@ namespace Silktide\QueueBall\ZeroMq;
 
 class SocketFactory
 {
-
     protected $context;
+    protected $sendTimeout;
 
-    public function __construct(\ZMQContext $context)
+    public function __construct(\ZMQContext $context, $sendTimeout=null)
     {
         $this->context = $context;
+        $this->sendTimeout = $sendTimeout;
     }
 
     public function createPushSocket()
     {
-        return new \ZMQSocket($this->context, \ZMQ::SOCKET_PUSH);
+        $zmqSocket = new \ZMQSocket($this->context, \ZMQ::SOCKET_PUSH);
+        if ($this->sendTimeout) {
+            $zmqSocket->setSockOpt(\ZMQ::SOCKOPT_SNDTIMEO, $this->sendTimeout);
+        }
+        return $zmqSocket;
     }
 
     public function createPullSocket()
